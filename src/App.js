eea30fe1bb427d6978ae6a8ffcbeb6f9a35dd020
Astra <abhaypratapsinghwd@gmail.com>
2024-06-18 14:01:59 +0530

    Added Sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import LeftSidebar from "./components/LeftSidebar/LeftSidebar";
+import MainContent from "./components/MainContent/MainContent";
 import Navbar from "./components/Navbar/Navbar";
+import RightSidebar from "./components/RightSidebar/RightSidebar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <div className="content">
+        <LeftSidebar />
+        <MainContent />
+        <RightSidebar />
+      </div>
     </div>
   );
 }
---
+++ src/components/RightSidebar/RightSidebar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./RightSidebar.css";
+
+const RightSidebar = () => {
+  return <div className="right-sidebar">RightSidebar</div>;
+};
+
+export default RightSidebar;
