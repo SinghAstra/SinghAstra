ed69305a5b09740e125523eff71c3f3ac367efdf
Astra <abhaypratapsinghwd@gmail.com>
2024-06-19 23:40:05 +0530

    Toggle Button

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -11,12 +11,18 @@ import {
   faVideo,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/images/logo.png";
 import user from "../../assets/images/profile-pic.png";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="nav">
       <div className="nav-left">
@@ -34,53 +40,59 @@ const Navbar = () => {
           <FontAwesomeIcon icon={faMagnifyingGlass} className="icons" />
           <input type="text" placeholder="Search" />
         </div>
-        <div className="nav-profile online">
+        <div className="nav-profile online" onClick={toggleMenu}>
           <img src={user} alt="user" />
         </div>
-        <div className="settings-menu">
-          <div className="settings-menu-header">
-            <img src={user} alt="user" className="icon" />
-            <div>
-              <p>John Nicolson</p>
-              <a href="/">Your Account</a>
+        {isMenuOpen && (
+          <div className="settings-menu">
+            <div className="settings-menu-header">
+              <img src={user} alt="user" className="icon" />
+              <div>
+                <p>John Nicolson</p>
+                <a href="/">Your Account</a>
+              </div>
+              <label className="switch">
+                <input type="checkbox" />
+                <span className="slider round"></span>
+              </label>
+            </div>
+            <div className="settings-menu-header">
+              <FontAwesomeIcon icon={faCommentDots} className="feedback-icon" />
+              <div>
+                <p>Give Feedback</p>
+                <a href="/">Help us to improve the new design.</a>
+              </div>
+            </div>
+            <div className="settings-menu-links">
+              <FontAwesomeIcon icon={faGear} className="main-icon" />
+              <div>
+                <p>Settings & Privacy</p>
+                <FontAwesomeIcon icon={faAngleRight} className="icon" />
+              </div>
+            </div>
+            <div className="settings-menu-links">
+              <FontAwesomeIcon icon={faLifeRing} className="main-icon" />
+              <div>
+                <p>Help & Support</p>
+                <FontAwesomeIcon icon={faAngleRight} className="icon" />
+              </div>
+            </div>
+            <div className="settings-menu-links">
+              <FontAwesomeIcon icon={faEye} className="main-icon" />
+              <div>
+                <p>Display & Accessibility</p>
+                <FontAwesomeIcon icon={faAngleRight} className="icon" />
+              </div>
+            </div>
+            <div className="settings-menu-links">
+              <FontAwesomeIcon icon={faDoorOpen} className="main-icon" />
+              <div>
+                <p>Logout</p>
+                <FontAwesomeIcon icon={faAngleRight} className="icon" />
+              </div>
             </div>
           </div>
-          <div className="settings-menu-header">
-            <FontAwesomeIcon icon={faCommentDots} className="feedback-icon" />
-            <div>
-              <p>Give Feedback</p>
-              <a href="/">Help us to improve the new design.</a>
-            </div>
-          </div>
-          <div className="settings-menu-links">
-            <FontAwesomeIcon icon={faGear} className="main-icon" />
-            <div>
-              <p>Settings & Privacy</p>
-              <FontAwesomeIcon icon={faAngleRight} className="icon" />
-            </div>
-          </div>
-          <div className="settings-menu-links">
-            <FontAwesomeIcon icon={faLifeRing} className="main-icon" />
-            <div>
-              <p>Help & Support</p>
-              <FontAwesomeIcon icon={faAngleRight} className="icon" />
-            </div>
-          </div>
-          <div className="settings-menu-links">
-            <FontAwesomeIcon icon={faEye} className="main-icon" />
-            <div>
-              <p>Display & Accessibility</p>
-              <FontAwesomeIcon icon={faAngleRight} className="icon" />
-            </div>
-          </div>
-          <div className="settings-menu-links">
-            <FontAwesomeIcon icon={faDoorOpen} className="main-icon" />
-            <div>
-              <p>Logout</p>
-              <FontAwesomeIcon icon={faAngleRight} className="icon" />
-            </div>
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
