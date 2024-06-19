bc3629588ed6a8922e7ee249461b390e443c1ec0
Astra <abhaypratapsinghwd@gmail.com>
2024-06-19 18:57:28 +0530

    Added Footer

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer/Footer";
 import LeftSidebar from "./components/LeftSidebar/LeftSidebar";
 import MainContent from "./components/MainContent/MainContent";
 import Navbar from "./components/Navbar/Navbar";
@@ -12,6 +13,7 @@ function App() {
         <MainContent />
         <RightSidebar />
       </div>
+      <Footer />
     </div>
   );
 }
