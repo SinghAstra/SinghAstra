8306e83bda0f49035f05eced47640b18492679f5
Astra <abhaypratapsinghwd@gmail.com>
2024-06-19 10:15:48 +0530

    Story

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./MainContent.css";
+import Story from "./Story";
 
 const MainContent = () => {
-  return <div className="main-content">MainContent</div>;
+  return (
+    <div className="main-content">
+      <Story />
+    </div>
+  );
 };
 
 export default MainContent;
---
+++ src/components/MainContent/Story.js
@@ -0,0 +1,36 @@
+import { faAdd } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import user2 from "../../assets/images/user2.png";
+import user3 from "../../assets/images/user3.png";
+import user4 from "../../assets/images/user4.png";
+import user5 from "../../assets/images/user5.png";
+
+const Story = () => {
+  return (
+    <div className="story-gallery">
+      <div className="story story1">
+        <FontAwesomeIcon icon={faAdd} className="icon" />
+        <p>Add Story</p>
+      </div>
+      <div className="story story2">
+        <img src={user2} alt="icon" className="icon" />
+        <p>Add Story</p>
+      </div>
+      <div className="story story3">
+        <img src={user3} alt="icon" className="icon" />
+        <p>Add Story</p>
+      </div>
+      <div className="story story4">
+        <img src={user4} alt="icon" className="icon" />
+        <p>Add Story</p>
+      </div>
+      <div className="story story5">
+        <img src={user5} alt="icon" className="icon" />
+        <p>Add Story</p>
+      </div>
+    </div>
+  );
+};
+
+export default Story;
