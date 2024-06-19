181ecc80ebdb1ccacd692ca16cfa31a96210fcc3
Astra <abhaypratapsinghwd@gmail.com>
2024-06-19 18:26:37 +0530

    Post

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./MainContent.css";
+import Post from "./Post";
 import Story from "./Story";
 import WritePost from "./WritePost";
 
@@ -8,6 +9,11 @@ const MainContent = () => {
     <div className="main-content">
       <Story />
       <WritePost />
+      <Post number={1} />
+      <Post number={2} />
+      <Post number={3} />
+      <Post number={4} />
+      <Post number={5} />
     </div>
   );
 };
