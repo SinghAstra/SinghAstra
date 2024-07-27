bc0c1d94a317c8372b0ec9708ca1e5d8675a45c1
Astra <abhaypratapsinghwd@gmail.com>
2024-07-27 11:48:15 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import About from "./components/About";
 import Features from "./components/Features";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
-import MouseFollower from "./components/MouseFollower";
 import Subscribe from "./components/Subscribe";
 
 function App() {
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/Footer.css";
 
 const Footer = () => {
+  const [currentTime, setCurrentTime] = useState("");
+
+  useEffect(() => {
+    const updateTime = () => {
+      const date = new Date();
+      const options = {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: true,
+      };
+      setCurrentTime(date.toLocaleTimeString([], options));
+    };
+
+    updateTime();
+    const interval = setInterval(updateTime, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="footer">
       <div className="footer-left">
-        <h5>2023 &copy;</h5>
-        <h5>0:05 AM EST</h5>
+        <h5>{new Date().getFullYear()} &copy;</h5>
+        <h5>{currentTime}</h5>
       </div>
       <div className="footer-right">
         <Link to="/">dribble</Link>
