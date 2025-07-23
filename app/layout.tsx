78b868184044e334445645ced258ee3569407ed2
Astra <abhaypratapsinghwd@gmail.com>
2025-07-23 16:26:48 +0530

    basic setup

---
+++ app/layout.tsx
@@ -0,0 +1,91 @@
+import Providers from "@/components/provider/provider";
+import { Toaster } from "@/components/ui/sonner";
+import { siteConfig } from "@/config/site";
+import type { Metadata } from "next";
+import NextTopLoader from "nextjs-toploader";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  keywords: [
+    "JavaScript",
+    "TypeScript",
+    "React JS",
+    "Express JS",
+    "Next JS",
+    "MongoDB",
+    "Mongoose",
+    "PostgreSQL",
+    "Prisma",
+    "Redis",
+    "Bull MQ",
+  ],
+  authors: [
+    {
+      name: "SinghAstra",
+      url: "https://github.com/SinghAstra",
+    },
+  ],
+  creator: "SinghAstra",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: "/assets/landing.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: ["/assets/landing.png"],
+    creator: "@singhastra",
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="antialiased min-h-screen ">
+        <Providers>
+          <NextTopLoader color="hsl(var(--primary))" showSpinner={false} />
+          {children}
+          <Toaster
+            theme="dark"
+            toastOptions={{
+              style: {
+                fontFamily: "Space Grotesk, monospace",
+                background: "hsl(var(--muted) / 0.2)",
+                color: "hsl(var(--foreground))",
+                border: "1px solid hsl(var(--border))",
+                letterSpacing: "0.01em",
+                fontSize: ".95rem",
+                backdropFilter: "blur(20px)",
+                WebkitBackdropFilter: "blur(20px)",
+              },
+            }}
+          />
+        </Providers>
+      </body>
+    </html>
+  );
+}
---
+++ config/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "@/interfaces/site";
+
+export const siteConfig: SiteConfig = {
+  name: "SinghAstra",
+  description:
+    "Portfolio showcasing projects and skills in JavaScript, TypeScript, React, Next.js, Node.js, and AI-powered GitHub tools.",
+  url: "https://singhastra.vercel.app",
+  links: {
+    twitter: "https://x.com/XSinghAstra",
+    github: "https://github.com/SinghAstra",
+    githubRepo: "https://github.com/SinghAstra/singhastra",
+  },
+};
