221bdfb60eb8b504c8283b2dc41f5f69f4295234
Astra <abhaypratapsinghwd@gmail.com>
2025-12-13 20:57:40 +0530

    Updt

--- config/site.ts
+++ config/site.ts
@@ -4,7 +4,7 @@ export const siteConfig: SiteConfig = {
   name: "SinghAstra",
   description: "Engineer in Making.",
   url: "https://singhastra.vercel.app",
-  keywords: ["Developer"],
+  keywords: ["Developer","React","Javascript"],
   links: {
     twitter: "https://x.com/XSinghAstra",
     github: "https://github.com/SinghAstra",
